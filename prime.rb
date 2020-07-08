5cf4d255f5e3e9a0955784d7d8fba17100fba6ea
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-08 22:37:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,11 @@ require "pry"
 
 def prime?(num)
   if num < 2
-    return true
+    return false
   else
     (2...num -1).to_a.each do |n|
       return false if num % n == 0
     end
   end
+  return true
 end
